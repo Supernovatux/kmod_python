5ec35162fc0b5430c13b59d5e3ca5916b0376beb
Quasar <98212676+quasar-nova@users.noreply.github.com>
2022-08-17 01:19:01 +0530

    Update Hello-2.c (Make it work)
    + Clang-format
    + Basic mem alloc on kernel level
    + Handle 1D array (only for now)

--- src/hello2.c
+++ src/hello2.c
@@ -1,61 +1,43 @@
-/* 
-
- * hello-2.c - Demonstrating the module_init() and module_exit() macros. 
-
- * This is preferred over using init_module() and cleanup_module(). 
-
- */ 
-
-#include <linux/init.h> /* Needed for the macros */ 
-#include <linux/random.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/ktime.h>
-#include <linux/kernel.h> /* Needed for pr_info() */ 
+#include <linux/module.h>
+#include <linux/random.h>
+#include <linux/slab.h>
 
-#include <linux/module.h> /* Needed by all modules */ 
-#define ARR_LEN_MY_MOD 1024
-MODULE_LICENSE("GPL"); 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("what-the-fuck-are-you-alone?!");
+// MODULE_AUTHOR("Supernovatux")
+MODULE_DESCRIPTION("learning");
 
-MODULE_AUTHOR("supernovatux"); 
+#define MEM 1024
+static int __init hello(void) {
+    ktime_t start_time, stop_time, elapsed_time;
+    int *A, *B, *C;
 
-MODULE_DESCRIPTION("learning");  
+    /* Don't allocate more on the stack, use kmalloc with GFP_USER */
+    A = kmalloc(MEM * MEM * sizeof(int), GFP_USER);
+    B = kmalloc(MEM * MEM * sizeof(int), GFP_USER);
+    C = kmalloc(MEM * MEM * sizeof(int), GFP_USER);
 
-static int __init hello(void) 
-{ 
-	ktime_t start_time, stop_time, elapsed_time;
-	start_time = ktime_get();
-	unsigned int A[ARR_LEN_MY_MOD][ARR_LEN_MY_MOD],B[ARR_LEN_MY_MOD][ARR_LEN_MY_MOD],C[ARR_LEN_MY_MOD][ARR_LEN_MY_MOD];
-	get_random_bytes(A,4*ARR_LEN_MY_MOD*ARR_LEN_MY_MOD);
-	get_random_bytes(B,4*ARR_LEN_MY_MOD*ARR_LEN_MY_MOD);
-	for(int i=0;i<ARR_LEN_MY_MOD;i++){
-		for(int j=0;j<ARR_LEN_MY_MOD;j++){
-			C[i][j]=0;
-			for(int k=0;k<ARR_LEN_MY_MOD;k++){
-				C[i][j]+=A[i][k]*B[k][j];
-			}
-		}
-	}
-	stop_time = ktime_get();
-	elapsed_time= ktime_sub(stop_time, start_time);
-	pr_info("elapsedTime : %lld\n",  ktime_to_ns(elapsed_time));
-    return 0; 
-} 
+    get_random_bytes(A, 4 * MEM * MEM);
+    get_random_bytes(B, 4 * MEM * MEM);
 
- 
+    start_time = ktime_get();
 
-static void __exit hello_exit(void) 
+    int i;
+    for (i = 0; i < MEM * MEM; i++) {
+        C[i] += A[i] * B[i];
+    }
 
-{ 
+    stop_time = ktime_get();
+    elapsed_time = ktime_sub(stop_time, start_time);
+    pr_info("elapsedTime : %lld\n", ktime_to_ns(elapsed_time));
+    return 0;
+}
 
-    pr_info("Goodbye, world 2\n"); 
+static void __exit hello_exit(void) { pr_info("Goodbye, world 2\n"); }
 
-} 
-
- 
-
-module_init(hello); 
-
-module_exit(hello_exit); 
-
- 
-
+module_init(hello);
+module_exit(hello_exit);
 MODULE_LICENSE("GPL");
